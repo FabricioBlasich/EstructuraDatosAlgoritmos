87dda81ef3f3c75a6e89abef1541a86a8900e001
Fabricio Blasich <fabricioblasich@gmail.com>
2023-04-19 20:26:22 -0300

    avances Tp4

--- src/practico5/Main.java
+++ src/practico5/Main.java
@@ -23,8 +23,24 @@ public class Main {
 
         pila1.mostrar();
 
-        System.out.println("\n\nTop: " + pila1.top());
+        System.out.println("\n\nTop: " + pila1.top() + "\n\n\n");
 
+        String infija1 = "a+b=";
+        String infija2 = "(a+b)-c=";
+        String infija3 = "a-(b+c)=";
+        String infija4 = "a+b*a=";
+        String infija5 = "a*(b+c)+a/e=";
+        String infija6 = "(b-a)/(c+d)=";
+        String infija7 = "a+b/(d-a)*e=";
+        
+        System.out.println("Infija: " + infija1 + " -----> Posfija: "+  infija_Posfija.Convertir(infija1));
+        System.out.println("Infija: " + infija2 + " ----->  Posfija: "+  infija_Posfija.Convertir(infija2));
+        System.out.println("Infija: " + infija3 + " ----->  Posfija: "+  infija_Posfija.Convertir(infija3));
+        System.out.println("Infija: " + infija4 + " ----->  Posfija: "+  infija_Posfija.Convertir(infija4));
+        System.out.println("Infija: " + infija5 + " ----->  Posfija: "+  infija_Posfija.Convertir(infija5));
+        System.out.println("Infija: " + infija6 + " ----->  Posfija: "+  infija_Posfija.Convertir(infija6));
+        System.out.println("Infija: " + infija7 + " ----->  Posfija: "+  infija_Posfija.Convertir(infija7));
+        
     }
 
 }
---
+++ src/practico5/infija_Posfija.java
@@ -0,0 +1,68 @@
+package practico5;
+
+public class infija_Posfija {
+    
+    public static String Convertir(String infija){
+        String posfija = "";
+        Pila pilaOperadores = new Pila<Character>();
+
+        while(!infija.isEmpty()){
+            char primeroInfija = infija.charAt(0);
+            if(Character.isLetter(primeroInfija))
+                posfija = posfija.concat(Character.toString(primeroInfija));
+            else if(pilaOperadores.esPilaVacia())
+                pilaOperadores.push(primeroInfija);
+            else{   
+                int operatorPriority = prioridadOperdador(primeroInfija);
+                if(operatorPriority == 0){
+                    while(!pilaOperadores.esPilaVacia()){
+                        posfija = posfija.concat(Character.toString((Character)pilaOperadores.top()));
+                        pilaOperadores.pop();
+                    }
+                }else if(!(operatorPriority == -1)){ // operatorPriority = ')'
+                    if(operatorPriority <= prioridadOperdador(((Character)pilaOperadores.top())) && operatorPriority != 1){
+                        while(!pilaOperadores.esPilaVacia() && operatorPriority <= prioridadOperdador(((Character)pilaOperadores.top())) &&  prioridadOperdador(((Character)pilaOperadores.top())) != 1){
+                            posfija = posfija.concat(Character.toString((Character)pilaOperadores.top()));
+                            pilaOperadores.pop();
+                        }
+                    }
+                    pilaOperadores.push(primeroInfija);
+                }else{
+                    while((Character)pilaOperadores.top() != '('){
+                        posfija = posfija.concat(Character.toString((Character)pilaOperadores.top()));
+                        pilaOperadores.pop();
+                    }
+                    pilaOperadores.pop();
+                }  
+                    
+            }
+        infija = infija.substring(1); 
+        }
+
+        return(posfija);
+    }
+
+
+    private static int prioridadOperdador(char operador){
+        switch (operador) {
+            case '=':
+                return 0;
+            case '(':
+                return 1;
+            case '+':
+            case '-':
+                return 2;
+            case '*':
+            case '/':
+                return 3;
+            default:
+                return -1; // ')'
+        }
+    }
+
+    public boolean esLetra(char c) {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+    }
+
+
+}
